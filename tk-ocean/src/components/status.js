5ff02c5faaa50af477013ff17d165311c7760374
Tk.FUISTELS <garcialuisfernando978@gmail.com>
2026-04-16 09:56:35 -0400

    Delete tk-ocean directory

--- tk-ocean/src/components/status.js
+++
@@ -1,7 +0,0 @@
-const statusElement = document.getElementById('status-label');
-
-function setStatus(message) {
-    if (statusElement) statusElement.textContent = message;
-}
-
-export { setStatus };
